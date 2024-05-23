3718435ca237cd3cb1c5bf184049c2beab7ec0f7
mario <mario_style_3088@hotmail.com>
2024-05-22 20:31:04 -0400

    primer commit

--- index.js
+++ index.js
@@ -12,55 +12,107 @@ const pool = new Pool(config)
 
 const insertAlumno = async () => {
 
-    const text = "INSERT INTO alumnos (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *"
+    // const text = "INSERT INTO alumnos (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *"
+    // const values = [process.argv[3], process.argv[4], process.argv[5], process.argv[6]]
     
-    const values = [process.argv[3], process.argv[4], process.argv[5], process.argv[6]]
-       
-    const response = await pool.query(text, values)
+    
+    try{
+        
+        const queryConfig = {
+            text: "INSERT INTO alumnos (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *",
+            values: [process.argv[3], process.argv[4], process.argv[5], process.argv[6]],
+            rowMode: "array"
+    }
+        const response = await pool.query(queryConfig)
+        
+        // console.log(`estudiante ${response.rows[3].nombre} agregado con exito`)
+    } catch (error) {
+        console.error("error status", error.code)
+    }
 
-    console.log(`estudiante ${response.rows[0].nombre} agregado con exito`)
-    console.log(response.rows)
+
+    // console.log(response.rows)
 }
 
 // insertAlumno()
 
 const consultaAlumnos = async () => {
-    const text = "SELECT * FROM alumnos"
+    try {
+        const queryConfig ={
+            text: "SELECT * FROM alumnos",
+            values: [],
+            rowMode: "array"
+        }
+        
+        const result = await pool.query(queryConfig)
     
-    const result = await pool.query(text)
-
     console.log(result.rows)
+    
+} catch (error) {
+    console.error("error status", error.code)
+    
 }
 
-consultaAlumnos()
+}
+
+// consultaAlumnos()
 
 const updateAlumno = async () => {
-    const text = "UPDATE alumnos SET nombre = $2 , curso = $3 , nivel = $4 WHERE rut = $1"
-    const values = [process.argv[4], process.argv[3], process.argv[5], process.argv[6]]
 
-    const result = await pool.query(text, values)
-    console.log(`estudiante ${values[1]} editado con exito`)
+    try {
+        const queryConfig ={
+            text : "UPDATE alumnos SET nombre = $2 , curso = $3 , nivel = $4 WHERE rut = $1",
+            values: [process.argv[4], process.argv[3], process.argv[5], process.argv[6]],
+            rowMode: "array"
+        }
+
+        const result = await pool.query(queryConfig)
+        console.log(`estudiante ${values[1]} editado con exito`)
+        
+    } catch (error) {
+        console.error("error status", error.code)       
+    }
+
 }
-
 // updateAlumno()
 
-const rutAlumno = async () =>{
-    const text = "SELECT * FROM alumnos WHERE rut = $1"
-    const values = [process.argv[4]]
+// rut - "12.345.678-9"
 
-    const result = await pool.query(text, values)
-    console.log(result.rows)
-    
+const rutAlumno = async () =>{
+
+    try {
+        const queryConfig ={
+            text: "SELECT * FROM alumnos WHERE rut = $1",
+            values: [process.argv[4]],
+            rowMode: "array"
+        }
+
+        const result = await pool.query(queryConfig)
+        console.log(result.rows)
+        
+    } catch (error) {
+        console.error("error status", error.code)
+        
+    }
 }
 
 // rutAlumno()
 
-const deleteAlumno = async () =>{
-    const text = "DELETE FROM alumnos WHERE rut = $1"
-    const values = [process.argv[4]]
+// eliminar  "Freddie Mercury" "9.876.543.21" vocalista 98
 
-    const result = await pool.query(text, values)
+const deleteAlumno = async () =>{
+    try {
+    const queryConfig = {
+        text: "DELETE FROM alumnos WHERE rut = $1",
+        values: [process.argv[4]]
+    }
+
+    const result = await pool.query(queryConfig)
     console.log(`Registro de estudiante con rut ${process.argv[4]} eliminado con exito`)
+    
+} catch (error) {
+    console.error("error status", error.code)
+}
 }
 
 // deleteAlumno()
